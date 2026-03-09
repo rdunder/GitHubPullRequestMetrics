61522873284987588da4e87de7f7dcfa2a82e821
rdunder <r.dunder@gmail.com>
2026-03-09 19:14:34 +0100

    initial project setup

---
+++ src/GitHubPullRequestMetrics/Models/GraphQL/Common/PageInfoDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubPullRequestMetrics.Models.GraphQL.Common
+{
+    internal class PageInfoDto
+    {
+    }
+}
